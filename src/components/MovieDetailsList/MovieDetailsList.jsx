618d5032b69390c6f0353053f848f5bc2997565d
Bogdan0109 <102528317+Bogdan0109@users.noreply.github.com>
2022-11-12 23:36:15 +0200

    finish

--- src/components/MovieDetailsList/MovieDetailsList.jsx
+++ src/components/MovieDetailsList/MovieDetailsList.jsx
@@ -2,14 +2,17 @@ import { MovieLink } from '../App.styled';
 
 const MovieDetailsList = () => {
   return (
-    <ul>
-      <li>
-        <MovieLink to="cast">Cast</MovieLink>
-      </li>
-      <li>
-        <MovieLink to="reviews">Review</MovieLink>
-      </li>
-    </ul>
+    <>
+      <h2>Additional information:</h2>
+      <ul>
+        <li>
+          <MovieLink to="cast">Cast</MovieLink>
+        </li>
+        <li>
+          <MovieLink to="reviews">Review</MovieLink>
+        </li>
+      </ul>
+    </>
   );
 };
 
--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,19 +1,22 @@
+import './MovieInfo.scss';
+
 const MovieInfo = ({ movies }) => {
   const { posterUrl, title, releaseYear, userScore, genresList, overview } =
     movies;
 
   return (
-    <div>
+    <div className="movie-info">
       <img src={posterUrl} alt={title} />
-      <h1>
-        {title} ({releaseYear})
-      </h1>
-      <p>User Score {userScore}%</p>
-      <h2>Overview:</h2>
-      <p>{overview}</p>
-      <h3>Genres:</h3>
-      <p>{genresList}</p>
-      <h2>Additional information:</h2>
+      <div>
+        <h1>
+          {title} ({releaseYear})
+        </h1>
+        <p>User Score {userScore}%</p>
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+        <h3>Genres:</h3>
+        <p>{genresList}</p>
+      </div>
     </div>
   );
 };
